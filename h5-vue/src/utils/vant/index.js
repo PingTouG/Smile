3497974585229542773ce1ee7f043c326f9147de
PingTouG <2798874773@qq.com>
2020-09-25 18:09:21 +0800

    chore(h5-vue): 引入Vant新组件及添加Vant内部的变量文件

--- h5-vue/src/utils/vant/index.js
+++ h5-vue/src/utils/vant/index.js
@@ -1,6 +1,11 @@
-import { Tabbar, TabbarItem } from 'vant'
+import { Tabbar, TabbarItem, Search, Swipe, SwipeItem, Image, Icon } from 'vant'
 
-export default Vue => {
-  Vue.use(Tabbar)
-  Vue.use(TabbarItem)
+export default app => {
+  app.use(Tabbar)
+  app.use(TabbarItem)
+  app.use(Search)
+  app.use(Swipe)
+  app.use(SwipeItem)
+  app.use(Image)
+  app.use(Icon)
 }
