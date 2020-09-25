2427bbd002729ef2c5b2b9aaf5f62f1f949eac49
PingTouG <2798874773@qq.com>
2020-09-25 18:07:50 +0800

    chore(h5-vue): 添加Gap组件和IconTag组件

---
+++ h5-vue/src/components/SGap/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="s-gap" />
+</template>
+
+<script setup>
+export default {
+  name: 'SGap',
+  props: {
+    color: {
+      type: String,
+      // utils/vant/var.less @gray-1
+      default: '#f7f8fa'
+    },
+    height: {
+      type: String,
+      default: '12px'
+    }
+  }
+}
+</script>
+
+<style lang="less" vars="{ color, height }" scoped>
+.s-gap {
+  background-color: var(--color);
+  height: var(--height);
+}
+</style>
---
+++ h5-vue/src/components/SIconTag/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="s-tag">
+    <van-image :src="icon" height="40" width="40" fit="cover" />
+    <div class="s-tag__text">{{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'STag',
+  props: {
+    icon: String,
+    text: String,
+    color: {
+      type: String,
+      //   utils/vant/var.less @gray-6
+      default: '#969799'
+    }
+  }
+}
+</script>
+
+<style lang="less" vars="{ color }" scoped>
+@import '~@/utils/vant/var';
+
+.s-tag {
+  display: inline-block;
+  text-align: center;
+  color: var(--color);
+}
+</style>
