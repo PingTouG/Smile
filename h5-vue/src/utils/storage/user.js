2c2f3ea3e0e0c5ae6dc85f5e56a1d9ab3f63b489
PingTouG <2798874773@qq.com>
2020-10-16 13:04:50 +0800

    fix(h5-vue): 修复设置用户本地缓存问题

--- h5-vue/src/utils/storage/user.js
+++ h5-vue/src/utils/storage/user.js
@@ -1,19 +1,19 @@
-const key = "SMILE_WEB_USER_INFO"
+const key = 'SMILE_WEB_USER_INFO'
 
 const set = payload => {
-    const userInfo = JSON.parse(payload ?? null)
-    window.localStorage.setItem(key,userInfo)
+  const userInfo = JSON.stringify(payload ?? undefined)
+  window.localStorage.setItem(key, userInfo)
 }
 
 const get = () => {
-    const userInfo = window.localStorage.getItem(key)
-    return JSON.stringify(userInfo ?? undefined)
+  const userInfo = window.localStorage.getItem(key)
+  return JSON.payload(userInfo ?? null)
 }
 
 const remove = () => window.localStorage.removeItem(key)
 
 export default {
-    set,
-    get,
-    remove
+  set,
+  get,
+  remove
 }
