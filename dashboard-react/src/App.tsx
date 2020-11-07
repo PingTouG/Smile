6547bffaaaaa0e830889e39ef87802403f926b61
PingTouG <2798874773@qq.com>
2020-11-07 20:54:38 +0800

    chore(dashboard-react): 添加sass

--- dashboard-react/src/App.tsx
+++ dashboard-react/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import router from './router'
 import {Spin} from './antd'
+import './styles/app.scss'
 
 function App() {
   return (
