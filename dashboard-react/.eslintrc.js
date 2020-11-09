c98d6693d23f188aaa142663b2716ff81a6706f2
PingTouG <2798874773@qq.com>
2020-11-09 14:43:50 +0800

    chore(dashboard-react): 添加Eslint

---
+++ dashboard-react/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-empty': 'off',
+  },
+}
