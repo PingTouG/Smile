731af667458f61dc419bd5ff8b62f52ea33c1124
PingTouG <2798874773@qq.com>
2020-11-11 21:16:30 +0800

    chore(dashboard-react): 添加recoil

--- dashboard-react/src/App.tsx
+++ dashboard-react/src/App.tsx
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import router from './router'
 import { Spin } from 'antd'
 import './styles/app.scss'
+import { RecoilRoot } from 'recoil'
 
 function App(): ReactElement {
   return (
-    <Router basename="/admin">
-      <Suspense fallback={<Spin />}>
-        <Switch>{router}</Switch>
-      </Suspense>
-    </Router>
+    <RecoilRoot>
+      <Router basename="/admin">
+        <Suspense fallback={<Spin />}>
+          <Switch>{router}</Switch>
+        </Suspense>
+      </Router>
+    </RecoilRoot>
   )
 }
 
